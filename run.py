459ce727efe59e7c0a6b7b573347562a0bee058e
PaulR1209 Russell <paulr711@gmail.com>
2024-02-16 15:47:34 +0000

    add splash screen and input option to start game or read instructions

--- run.py
+++ run.py
@@ -1,38 +1,45 @@
 import random
 from words import list_of_words
 
-def introduction():
+def splash_screen():
+    print(r"""
+    
+██   ██  █████  ███    ██  ██████  ███    ███  █████  ███    ██ 
+██   ██ ██   ██ ████   ██ ██       ████  ████ ██   ██ ████   ██ 
+███████ ███████ ██ ██  ██ ██   ███ ██ ████ ██ ███████ ██ ██  ██ 
+██   ██ ██   ██ ██  ██ ██ ██    ██ ██  ██  ██ ██   ██ ██  ██ ██ 
+██   ██ ██   ██ ██   ████  ██████  ██      ██ ██   ██ ██   ████ 
+                                                                
+    """)
+
+    options = input('Press 1 to start game. Press 2 for instructions.')
+    if options == '1':
+        return display_word(word, user_input)
+    elif options == '2':
+        return instructions()
+    else:
+         print('Invalid input. Press 1 to start, or press 2 for instructions')      
+
+def instructions():
     """
-    Welcomes the user and explains the rules
+    When you click '2' on the splash screen page, this displays,
+    displaying instructions of the game
     """
     print('Thank you for choosing to play Hangman!\n')
     print('The objective of the game is simple!\n')
-    print('It is to guess the what the hidden word is before the stick figure is hung.\n')
+    print('It is to guess what the hidden word is before the stick figure is hung.\n')
     print('Start guessing letters when prompted')
-    print('Choose your letters wisely, you have 8 lives!\n')
+    print('Choose your letters wisely, you have 7 lives!\n')
     print('The game ends when you either guess the correct word or you run out of lives.\n')
     
-    start_game = input('Type "start" to begin game: ')
-
-    if start_game != 'start':
-        print('Invalid input. Type "start" to begin game')
-    
-
 def get_word():
     """
     Generates a random word from the list_of_words.
     """
     word = random.choice(list_of_words)
-    return word
-
-def display_word(word, user_input):
-    """
-    Displays hidden letters in hidden word as underscores
-    and reveals letters that are guessed correctly
-    """            
+    return word           
 
 def main():
-    introduction()
-    get_word()
+    splash_screen()
 
 main()         
