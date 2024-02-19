93bdb7fde51d92b3d4581f8967aac54ae48ee5a5
PaulR1209 Russell <paulr711@gmail.com>
2024-02-19 21:32:45 +0000

    edit run.py to be pep8 compliant

--- run.py
+++ run.py
@@ -44,7 +44,7 @@ def main_menu():
     while True:
         try:
             options = (input('Press 1 to start game. '
-                                'Press 2 for instructions: '))
+                             'Press 2 for instructions: '))
 
             if options == '1':
                 clear_screen()
@@ -59,7 +59,7 @@ def main_menu():
                     f'{Fore.RED}Only 1 or 2 are allowed, '
                     f'try again.{Fore.RESET}'
                     )
-                    
+
         except ValueError as e:
             clear_screen()
             print(e)
