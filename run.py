05333ed2b9d23402006387deb475fef840ea10fa
PaulR1209 Russell <paulr711@gmail.com>
2024-02-19 12:10:17 +0000

    add display text in front of guessed letter list

--- run.py
+++ run.py
@@ -167,7 +167,7 @@ def run_game():
         clear_screen()
         print('Lets play!\n')
         print(f'Lives: {remaining_attempts}\n')
-        print(' '.join(guessed_letter_list))
+        print('Guessed Letters: ' + ' '.join(guessed_letter_list))
         print(result_message)
         print(hangman_stages(remaining_attempts))
 
